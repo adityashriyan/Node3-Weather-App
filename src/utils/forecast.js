9a80c6a83c39cd3a1d716b056cda9e1868f6e498
Aditya Shriyan <adtyshryn@gmail.com>
2019-12-23 16:31:31 +0530

    Modified API call to return a smaller response packet

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -1,7 +1,8 @@
 const request = require('request')
 
 const forecast = (latitude, longitude, callback) => {
-    const url = 'https://api.darksky.net/forecast/a813af19395c90a7608369cada86f2fd/'+latitude+','+longitude+'?units=si'
+    const url = 'https://api.darksky.net/forecast/a813af19395c90a7608369cada86f2fd/'+latitude+','+longitude+'?units=si&exclude=[minutely,daily,flags]'
+    console.log(latitude + "," + longitude);
 
     request({url, json: true}, (error, {body}) => {
 
@@ -10,7 +11,7 @@ const forecast = (latitude, longitude, callback) => {
         } else if (body.error) {
             callback('Unable to find location', undefined)
         } else {
-            const forecast_string = body.daily.summary + ' It is currently ' + body.currently.temperature + ' degrees out. There is a ' + body.currently.precipProbability + '% chance of rain. If it does rain though (I can\'t see the future na..), it will do so with a ' + body.currently.precipIntensity + '% intensity'
+            const forecast_string = body.hourly.summary + ' It is currently ' + body.currently.temperature + '\xB0C out. There is a ' + body.currently.precipProbability + '% chance of rain. If it does rain though (I can\'t see the future na..), it will do so with a ' + body.currently.precipIntensity + '% intensity'
             callback(undefined, forecast_string)
         }
     })
